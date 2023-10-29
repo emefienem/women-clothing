f01c9fbe654469332b6f33ec9ec4fa078ca1feff
emefienem1 <michael.emefienem@gmail.com>
2023-10-29 11:58:00 +0100

    Added new features

--- js/shop.js
+++ js/shop.js
@@ -1,189 +1,88 @@
-const product = [
-    {
-        id: 0,
-        image: 'images/Clothes3.jpg',
-        title: 'Gown Black Dress',
-        price: 10,
-    }, 
-    {
-        id: 1,
-        image: 'images/Clothes1.jpg',
-        title: 'Gown Brown Dress',
-        price: 3, 
-    },
-    {
-        id: 2,
-        image: 'images/Clothes2.jpg',
-        title: 'Gown White Dress',
-        price: 13,
-    }, 
-    {
-        id: 3,
-        image: 'images/Clothes1.jpg',
-        title: 'Dress',
-        price: 8,
-    }, 
-    {
-        id: 4,
-        image: 'images/Clothes3.jpg',
-        title: 'Gown Black Dress',
-        price: 8,
-    },
-    {
-        id: 5,
-        image: 'images/Clothes1.jpg',
-        title: 'Gown Brown Dress',
-        price: 11,
-    },
-    {
-        id: 6,
-        image: 'images/Clothes2.jpg',
-        title: 'Gown white Dress',
-        price: 6,
-    },
-    {
-        id: 7,
-        image: 'images/Clothes1.jpg',
-        title: 'Dress',
-        price: 8,
-    },
-    {
-        id: 8,
-        image: 'images/Clothes3.jpg',
-        title: 'Gown Black Dress',
-        price: 4,
-    },
-    {
-        id: 9,
-        image: 'images/Clothes1.jpg',
-        title: 'Gown Brown Dress',
-        price: 6,
-    },
-    {
-        id: 10,
-        image: 'images/Clothes2.jpg',
-        title: 'Gown White Dress',
-        price: 7,
-    },
-    {
-        id: 11,
-        image: 'images/Clothes1.jpg',
-        title: 'Dress',
-        price: 12, 
-    }
-];
+const cartCloseBtn = document.querySelector('.close');
+const cartBox = document.querySelector('.cartBox');
+const iconShopping = document.getElementById('cartShowButton')
 
+iconShopping.addEventListener('click', function() {
+    document.getElementById('confirmationModal').style.display = 'block'
+});
+cartCloseBtn.addEventListener('click', function() {
+    document.getElementById('confirmationModal').style.display = 'none';
+});
 
-const categories = [...new Set(product.map((item) => 
-    {return item}))]
-    let i = 0;
-document.getElementById('root').innerHTML = categories.map((item) => {
-    const { image, title, price } = item;
-    return (
-        `<div class='box'>
-            <div class='img-box'>
-                <img class='images' src=${image}></img>
-            </div>
-        <div class='bottom'>
-        <p>${title}</p>
-        <h2>$${price}.00</h2>` +
-        "<button onclick='addToCart("+(i++)+")' class='addCartButton'>Add to cart</button>" +
-        `</div>
-        </div>`
-        
-    )
-}).join('');
-
-const shoppingCart = [];
-
-// Function to save the shopping cart to local storage
-function saveCartToLocalStorage() {
-    localStorage.setItem('shoppingCart', JSON.stringify(shoppingCart));
-  }
-  
-  // Function to load the shopping cart from local storage
-  function loadCartFromLocalStorage() {
-    const savedCart = localStorage.getItem('shoppingCart');
-    if (savedCart) {
-      shoppingCart.push(...JSON.parse(savedCart));
-      displayCart();
-    }
-  }
-  
-  // Call loadCartFromLocalStorage when the page loads
-  window.addEventListener('load', loadCartFromLocalStorage);
-
-  const buttons = document.querySelectorAll('.addCartButton')
-//   buttons.forEach(button => {
-//     button.addEventListener('click', function () {
-//         if(button.textContent === 'Add to cart') {
-//             button.textContent = 'Added'
-//         } else{
-//             button.textContent = 'Add to cart'
-//         }
-//     })
-//   })
-
-// Function to add an item to cart
-    function addToCart(a) {
-      shoppingCart.push({ ...categories[a] });
-      saveCartToLocalStorage(); // Save the updated cart to local storage
-      displayCart();
-    }
-
-  // Function to delete an item from the cart
-  function delElement(a) {
-    shoppingCart.splice(a, 1);
-    saveCartToLocalStorage(); // Save the updated cart to local storage
-    displayCart();
-  }
-  
-  // Function to clear the cart and local storage
-  function clearCart() {
-    shoppingCart.length = 0;
-    localStorage.removeItem('shoppingCart');
-    displayCart();
-  }
-
-// Function to update the cart item quantity and recalculate the total
-function updateQuantity(a, newQuantity) {
-    shoppingCart[a].quantity = newQuantity;
-    saveCartToLocalStorage(); // Save the updated cart to local storage
-    displayCart();
-  }
-  
-
-function displayCart() {
-    let j = 0;
-    let total = 0;
-    document.getElementById('raised').innerHTML = shoppingCart.length;
-    if(shoppingCart.length === 0) {
-        document.getElementById('cartItem').innerHTML = 'Your cart is empty';
-        document.getElementById('total').innerHTML = '$ '+0+'.00';
-    } 
-    else{
-        document.getElementById('cartItem').innerHTML = shoppingCart.map((items, index) => {
-            const { image, title, price, quantity } = items;
-            // const itemTotal = price * quantity;
-            // total += itemTotal;
-            total += price;
-            document.getElementById('total').innerHTML = '$ '+total+'.00';
-            return(
-
-                `<div class='cart-item'>
-                <div class='row-img'>
-                    <img class='rowing' src=${image}>
-                </div>
-                <p style='font-size:12px;'>${title}</p>
-                <input type="number" id="quantity" name="quantity" min="1" max="5" value="${quantity}" class="quantity-input">
-                <h2 style='font-size: 15px;'> $${price}.00</h2>` +
-                "<i class='fa-solid fa-trash' onclick='delElement("+ (j++) +")'></i></div>"
-            );      
-        }).join('');
-    }
+const cartButton = document.querySelectorAll('.addCartButton');
+let enrobeitems = [];
+for(let i = 0; i < cartButton.length; i++) {
+    cartButton[i].addEventListener('click', function (e) {
+        const productContainer = e.target.parentElement;
+        const productImage = productContainer.querySelector('img');
+        const productName = productContainer.querySelector('.bottom p').textContent;
+        const productPrice = parseFloat(productContainer.querySelector('.bottom h2').textContent.replace('$', ''));
+        console.log(productName, productPrice, productImage)
+        if(typeof(Storage) !== 'undefined') {
+            let item = {
+                id: i + 1, 
+                name: productName,
+                price: productPrice,
+                no: 1,
+                image: productImage,
+            };
+            if(localStorage.getItem('enrobeitems') === null) {
+                enrobeitems.push(item);
+                localStorage.setItem('enrobeitems', JSON.stringify(enrobeitems));
+                window.location.reload();
+            }else{
+                const localItems = JSON.parse(localStorage.getItem('enrobeitems'));   
+                localItems.map(data => {
+                    if(item.id == data.id) {
+                        item.no = data.no + 1;
+                    } else{
+                        enrobeitems.push(data)
+                    }
+                });
+                enrobeitems.push(item);
+                localStorage.setItem('enrobeitems', JSON.stringify(enrobeitems));
+                window.location.reload();
+            }
+        } else{
+            alert('local storage is not working on your browser')
+        }
+    
+    })
 }
+const cartSection = document.getElementById('cart');
+const cartBadge = document.querySelector('#raised');
+const cartBadges = document.querySelector('.numberBadges');
+let no = 0;
+JSON.parse(localStorage.getItem('enrobeitems')).map(data => {
+    no += data.no;
+})
+cartBadge.innerHTML = no;
+cartBadges.innerHTML = no;
 
-const images = document.querySelectorAll('.slide');
+const cartTable = cartSection.querySelector('table');
+let tableData = ''
+tableData += `  
+    <thead>
+        <tr>
+            <td>S no.</td>
+            <td>Image</td>
+            <td>Product</td>
+            <td>Price</td>
+            <td>Quantity</td>
+            <td>Remove</td>
+        </tr>
+    </thead>`;
+if(JSON.parse(localStorage.getItem('enrobeitems')) === null) {
+    tableData += '<tbody><tr><td>No items found</td></tr></tbody>';
+} else {
+    JSON.parse(localStorage.getItem('enrobeitems')).map(data => {
+        tableData +=
+        '<tbody><tr><td>'+data.id+'</td><td>'+data.image+'</td><td>'+data.name+'</td><td>'+data.price+'</td><td><input type="number" value="1"></td><td><a href="#" onclick=Delete(this);><i class="circle fa fa-times-circle"></i></a></td></tr></tbody>'
+    });
+}
+cartTable.innerHTML = tableData;
+
+
 const active = document.querySelector('.active');
 
 if(active) {
